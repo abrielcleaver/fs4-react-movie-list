ed174aa80c32f161dd3c3b9db7c3f999c87ff899
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-06 07:54:51 -0700

    Update test to check for text in header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/abriel's movie poster creator/i);
+  expect(headerText).toBeInTheDocument();
 });
