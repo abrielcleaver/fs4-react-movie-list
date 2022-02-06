b78a971e9dc9e777a80ce0e642ebde3a29077f7e
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-06 07:25:12 -0700

    Update Movie component name to MovieItem

--- src/Components/Movies/Movie.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Movie({ titleForm, directorForm, yearForm }) {
-  return <div className='movie-item'>
-    <h1>{titleForm}</h1>
-    <h2>Year Released: {yearForm}</h2>
-    <h2>Director: {directorForm}</h2>
-  </div>;
-}
