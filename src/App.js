bb0443f2be6215a39748c7d19f5043826c4420fc
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-05 20:16:23 -0700

    Import the movie list, form, and item components in App and track state for allMovies. import useMovieForm to track state for form. components render on page but information is not yet updated on submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
-import MovieList from './Components/MovieList';
+import { useState } from 'react';
+import { useMovieForm } from './Components/Movies/useMovieForm';
+import MovieForm from './Components/Movies/MovieForm';
+import MovieList from './Components/Movies/MovieList';
+import Movie from './Components/Movies/Movie';
 
 function App() {
+  // tracks state for `allMovies`,  `filteredMovies  `movieFormYearReleased`, `movieFormDirector`, `movieTitle`, `movieFormColor`; 
+  const [allMovies, setAllMovies] = useState([]);
+  // const [filteredMovies, setFilteredMovies] = useState('');
+ 
+  const {
+    titleForm, setTitleForm,
+    directorForm, setDirectorForm,
+    yearForm, setYearForm,
+    colorForm, setColorForm, 
+  } = useMovieForm('');
+
+  function addMovie(newMovie) {
+    const updatedMovies = [...allMovies, newMovie];
+
+    setAllMovies(updatedMovies);
+  }
   return (
     <div className="App">
-      <MovieList />
+      <div>
+        <MovieForm 
+          titleForm={titleForm}
+          setTitleForm={setTitleForm}
+          directorForm={directorForm}
+          setDirectorForm={setDirectorForm}       
+          yearForm={yearForm}
+          setYearForm={setYearForm}
+          colorForm={colorForm}
+          setColorForm={setColorForm}    
+        />
+
+  
+        <Movie 
+          title={titleForm}
+          director={directorForm}
+          year={yearForm}
+          color={colorForm}        
+        />
+        <p>Filter Movies</p>
+        <input/>
+
+        <MovieList allMovies={allMovies}/>
+
+      </div>
     </div>
   );
 }
