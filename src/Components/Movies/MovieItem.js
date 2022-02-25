6e1319842a6bdce9065234e46b075077530841f5
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-25 12:39:18 -0700

    Add handleDeleteMovie to MovieList and MovieItem. movie does not delete yet

--- src/Components/Movies/MovieItem.js
+++ src/Components/Movies/MovieItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function MovieItem({ color, title, director, year }) {
-  return <div className='movie-item' style={{ background: color }}>
+export default function MovieItem({ 
+  color, 
+  title, 
+  director, 
+  year, 
+  handleDeleteMovie 
+}) {
+  return <div
+    onClick={() => handleDeleteMovie ? handleDeleteMovie(title) : null} 
+    className='movie-item' style={{ background: color }}>
     <h3>{title}</h3>
     <p>{year}</p>
     <p>{director}</p>
